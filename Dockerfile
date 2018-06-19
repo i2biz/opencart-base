70541b9300c7dfa6eefce44db37ce47f6ca09d6e
Jacek Bzdak <jbzdak@gmail.com>
2018-06-19 12:52:06 +0200

    Intial commit.

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM php:7.2.6-apache-stretch
+
+COPY setup-system.sh /opt/setup-system.sh
+RUN /opt/setup-system.sh
+
+VOLUME /var/www/html/
+VOLUME /usr/local/etc/php
---
+++ setup-system.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+apt-get update -q
+apt-get upgrade -qy
+
+apt-get install -qy libcurl4-openssl-dev zlib1g-dev unzip libgd-dev libjpeg-dev libpng-dev libfreetype6-dev netcat-traditional nano vim libmcrypt-dev mcrypt
+
+docker-php-ext-configure gd --with-jpeg-dir=/usr/lib --with-freetype-dir=/usr/lib
+
+docker-php-ext-install curl zip gd
+
+
+rm -rf /var/lib/apt/lists/
+apt-get clean
