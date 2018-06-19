e7d5b8321f2bc49ffd0fcedfebd8778f62e20267
Jacek Bzdak <jbzdak@gmail.com>
2018-06-19 13:37:36 +0200

    Removed unneccessary zip distro.

--- setup-system.sh
+++ setup-system.sh
@@ -8,7 +8,6 @@ apt-get upgrade -qy
 apt-get install -qy libcurl4-openssl-dev zlib1g-dev unzip libgd-dev libjpeg-dev libpng-dev libfreetype6-dev netcat-traditional nano vim libmcrypt-dev mcrypt
 
 docker-php-ext-configure gd --with-jpeg-dir=/usr/lib --with-freetype-dir=/usr/lib
-# docker-php-ext-configure zip --with-libzip=/usr/lib/x86_64-linux-gnu
 
 docker-php-ext-install curl zip gd
 
